80a2d68eb288f9ab1e1385f392a594ab7b7228e7
Martand Singh <MSingh@dohms.gov.ae>
2020-01-26 22:33:43 +0400

    INITIAL COMMIT

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserInfo.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
